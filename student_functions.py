5862bfceda528ebcdc02fec59e1490015c0965b0
Thái Hiệp <thaihiep232002@gmail.com>
2023-02-06 18:38:36 +0700

    Update student_functions.py

--- student_functions.py
+++ student_functions.py
@@ -1,5 +1,12 @@
+#full name: Nguyễn Thái Hiệp
+#id: 20127496
+
 import numpy as np
-from PriorityQueue import PriorityQueue
+from queue import PriorityQueue
+from queue import Queue
+import math
+
+INF = 999999
 
 def DFS(matrix, start, end):
     """
@@ -30,10 +37,13 @@ def DFS(matrix, start, end):
     n_vertices = matrix.shape[0]
     checked = [False for i in range(n_vertices)]
 
-    frontier.append(start)
-    
+    frontier.append((-1,start))
     while frontier:
-        node = frontier.pop()
+        print(frontier)
+        buffer, node = frontier.pop()
+
+        if checked[node] == True : continue
+        visited.update({node: buffer})
         checked[node] = True
         if node == end:
             while node != start:
@@ -45,9 +55,8 @@ def DFS(matrix, start, end):
 
         for i in range(n_vertices - 1, -1, -1):
             if matrix[node][i] != 0 and checked[i] == False:
-                frontier.append(i)
-                visited[i] = node
-    print(path)
+                frontier.append((node, i))
+
     return visited, path
 
 def BFS(matrix, start, end):
@@ -75,17 +84,20 @@ def BFS(matrix, start, end):
     
     path=[]
     visited={}
-   
 
-    frontier = []
+    frontier = Queue()
     n_vertices = matrix.shape[0]
     checked = [False for i in range(n_vertices)]
-
-    frontier.append(start)
-    
-    while frontier:
-        node = frontier.pop(0)
+ 
+    frontier.put((-1, start))
+    while not frontier.empty():
+        node_buffer, node = frontier.get()
+        
+        if checked[node] == True : continue
+            
+        visited[node] = node_buffer
         checked[node] = True
+
         if node == end:
             while node != start:
                 path.append(node)
@@ -96,11 +108,9 @@ def BFS(matrix, start, end):
 
         for i in range(n_vertices):
             if matrix[node][i] != 0 and checked[i] == False:
-                frontier.append(i)
-                visited[i] = node
-    print(path)
+                frontier.put((node, i))
+    
     return visited, path
-
 
 def UCS(matrix, start, end):
     """
@@ -123,38 +133,73 @@ def UCS(matrix, start, end):
         Founded path
     """
     # TODO:  
-    # path=[]
-    # visited={}
-
-    # frontier = PriorityQueue()
-
-    # n_vertices = matrix.shape[0]
-    # checked = [False for i in range(n_vertices)]
-
-    # frontier.insert((start, 0))
     
-    # while frontier:
-    #     node = frontier.delete()
+    path=[]
 
-    #     vertex = node[0]
-    #     cost = node[1]
-    #     checked[vertex] = True
-    #     if vertex == end:
-    #         while vertex != start:
-    #             path.append(vertex)
-    #             vertex = visited[vertex]
-    #         path.append(start)
-    #         path.reverse()
-    #         break
+    visited={}
+    global INF
 
-    #     for i in range(n_vertices):
-    #         if matrix[vertex][i] != 0 and checked[i] == False:
-    #             frontier.insert((i, matrix[vertex][i]))
-    #             visited[i] = vertex
-    # print(path)
+    frontier = PriorityQueue()
+
+    n_vertices = matrix.shape[0]
+
+    checked = [False for i in range(n_vertices)]
+    explored = [INF for i in range(n_vertices)]
+    frontier.put((0,-1, start))
+    
+    while not frontier.empty():
+        currentWeight, buffer, vertex = frontier.get()
+
+        explored[vertex] = -INF
+        if checked[vertex] == True : continue
+        checked[vertex] = True
+        
+        visited.update({vertex: buffer})
+        if vertex == end:
+            while vertex != start:
+                path.append(vertex)
+                vertex = visited[vertex]
+            path.append(start)
+            path.reverse()
+            break
+        
+        for edge in range(n_vertices):
+            if matrix[vertex][edge] != 0 and checked[edge] == False:
+                weight = currentWeight + matrix[vertex][edge]
+                if weight < explored[edge]:
+                    explored[edge] = weight
+                    frontier.put((explored[edge],vertex, edge))
 
     return visited, path
 
+# Heuristic of Greedy function
+def H(matrix, start):
+
+    frontier = PriorityQueue()
+
+    n_vertices = matrix.shape[0]
+
+    checked = [False for i in range(n_vertices)]
+    Heuristic = [0 for i in range(n_vertices)]
+    explored = [INF for i in range(n_vertices)]
+    frontier.put((0,-1, start))
+    
+    while not frontier.empty():
+        currentWeight, buffer, vertex = frontier.get()
+
+        Heuristic[vertex] = currentWeight
+        explored[vertex] = -INF
+        if checked[vertex] == True : continue
+        checked[vertex] = True
+        
+        for edge in range(n_vertices):
+            if matrix[vertex][edge] != 0 and checked[edge] == False:
+                weight = currentWeight + matrix[vertex][edge]
+                if weight < explored[edge]:
+                    explored[edge] = weight
+                    frontier.put((explored[edge],vertex, edge))
+
+    return Heuristic
 
 def GBFS(matrix, start, end):
     """
@@ -176,10 +221,47 @@ def GBFS(matrix, start, end):
     path: list
         Founded path
     """
+    
     # TODO: 
     path=[]
     visited={}
+
+    global INF
+
+    Heuristic = H(matrix, end)
+    frontier = PriorityQueue()
+
+    n_vertices = matrix.shape[0]
+
+    frontier.put((0,-1, start))
+    
+    while not frontier.empty():
+        ignore, buffer, vertex = frontier.get()
+        visited.update({vertex: buffer})
+        if vertex == end:
+            while vertex != start:
+                path.append(vertex)
+                vertex = visited[vertex]
+            path.append(start)
+            path.reverse()
+            break
+        
+        for edge in range(n_vertices):
+            if matrix[vertex][edge] != 0:
+                frontier.put((Heuristic[edge],vertex, edge))
+
     return visited, path
+
+
+def Heuristic(pos, end, n_vertices):
+    x_end = pos[end][0]
+    y_end = pos[end][1]
+    H = [0 for i in range(n_vertices)]
+    for i in pos:
+        x_i = pos[i][0]
+        y_i = pos[i][1]
+        H[i] = math.sqrt((x_i - x_end)*(x_i - x_end) + (y_i - y_end)*(y_i - y_end))
+    return H
 
 def Astar(matrix, start, end, pos):
     """
@@ -205,5 +287,39 @@ def Astar(matrix, start, end, pos):
     # TODO: 
     path=[]
     visited={}
-    return visited, path
 
+    global INF
+
+
+    frontier = PriorityQueue()
+
+    n_vertices = matrix.shape[0]
+
+    H = Heuristic(pos, end, n_vertices)
+
+    checked = [False for i in range(n_vertices)]
+    explored = [INF for i in range(n_vertices)]
+    frontier.put((0, -1, start))
+    
+    while not frontier.empty():
+        currentWeight,buffer, vertex = frontier.get()
+        if checked[vertex] == True: continue
+        explored[vertex] = -INF
+        checked[vertex] = True
+        visited.update({vertex: buffer})
+        if vertex == end:
+            while vertex != start:
+                path.append(vertex)
+                vertex = visited[vertex]
+            path.append(start)
+            path.reverse()
+            break
+        
+        for edge in range(n_vertices):
+            if matrix[vertex][edge] != 0 and checked[edge] == False:
+                weight = currentWeight + matrix[vertex][edge] + H[edge]
+                if weight < explored[edge]:
+                    explored[edge] = weight
+                    frontier.put((explored[edge],vertex, edge))
+
+    return visited, path
